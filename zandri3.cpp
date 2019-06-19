6f8fcc4896bd96eeaaf2234129291ff0c14699ef
Zandri12 <zandri081@gmail.com>
2019-06-19 13:51:19 +0700

    latihan5

---
+++ zandri3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int nilai;
+	cout<<"6: ";
+	cin>>nilai;
+
+if (nilai > 0)
+   cout<<"nilai yang anda masukan adalah bilangan positif";
+
+   return 0;
+}
