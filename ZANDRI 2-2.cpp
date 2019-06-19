37283d121d493714473d0a47930624174f35c7e5
Zandri12 <zandri081@gmail.com>
2019-06-19 13:01:11 +0700

    latihan3

---
+++ ZANDRI 2-2.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+	char Teks [20];
+	int X;
+	cout<<"HARRY POTTER : "; cin>>Teks;
+	cout<<"20 : "; cin>>X;
+	cout<<Teks<<endl;  // bisa ditulis dengan cout<<X<<'\n';
+	cout<<X;
+	return 0;
+}
