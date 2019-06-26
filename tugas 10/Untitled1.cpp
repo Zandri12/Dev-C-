cd5006fa1f685be2eb110996763536ed6ad8a26c
Zandri12 <zandri081@gmail.com>
2019-06-26 09:10:39 +0700

    10

---
+++ tugas 10/Untitled1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // deklarasi variabel
+  int i;
+  double d;
+  char c;
+  char *s;
+  
+  // mengisu nilai ke dalam variabel
+  i = 88;
+  d = 4.675;
+  c = 'C';
+  s = (char *) "contoh string";
+  
+  // menampilkan nilai variabel
+  cout<<"nilai i: "<<i<<endl;
+  cout<<"nilai d: "<<d<<endl;
+  cout<<"nilai c: "<<c<<endl;
+  cout<<"nilai s: "<<s<<endl;
+  
+  return 0;
+}
